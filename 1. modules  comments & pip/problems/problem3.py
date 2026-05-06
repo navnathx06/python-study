8b4dd7770df223d63c95a60561c5fb4d48f5e2be
Navnath <navnyajade@gmail.com>
2026-05-06 19:01:06 +0530

    Delete 1. modules  comments & pip directory

--- 1. modules  comments & pip/problems/problem3.py
+++
@@ -1,4 +0,0 @@
-# Q. Install an external module and use it to perform an operation of your interest.
-
-import pyttsx3
-pyttsx3.speak("I will speak this text")
--- 1. modules  comments & pip/theory/modules.py
+++
@@ -1,4 +0,0 @@
-import math
-
-print(math.sqrt(25))   # 5.0
-print(math.pi)         # 3.14159...
