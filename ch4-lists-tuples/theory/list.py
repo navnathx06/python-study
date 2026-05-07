dc9e191e93bbc84f6b4e559a4e574dd8e14215f1
Navnath <navnyajade@gmail.com>
2026-05-07 18:05:49 +0530

    Add ch4 theory

---
+++ ch4-lists-tuples/theory/list.py
@@ -0,0 +1,7 @@
+a = ["apple",1,3,3.5,"friend",False,]
+
+print(a[0])   # print apple
+a[0] = "grapes"   # print grapes
+
+print(a[0])
+print(a[1:4])
---
+++ ch4-lists-tuples/theory/tuple.py
@@ -0,0 +1,8 @@
+# TUPLE - A tuple is a built-in data type used to store a collection of items in a single variable. 
+# It is similar to a list, but with one key difference:
+# 👉 Tuples are immutable (they cannot be changed after creation).
+
+a = (2,3,5,"helllo","good")
+print(a)
+
+print(type(a))
