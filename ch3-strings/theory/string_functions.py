ba502233bfc728df2155b55c1e083a3887081d38
Navnath <navnyajade@gmail.com>
2026-05-09 22:01:44 +0530

    Add ch3 theory

---
+++ ch3-strings/theory/string_functions.py
@@ -0,0 +1,10 @@
+name = "navnath"
+
+print(len(name))
+
+print(name.endswith("nath"))#true
+print(name.startswith("nav"))#true
+
+print(name.capitalize()) # make first letter capital
+
+print("navnath".upper()) # make uppercase
